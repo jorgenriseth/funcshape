331673701dfd0d5668a30fd9731e6a6132013ff5
Jørgen Riseth <jnriseth@gmail.com>
2022-07-26 12:49:18 +0200

    bugfix in download script

--- download-mnist.sh
+++ download-mnist.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 curl --create-dirs -o data/t10k-images-idx3-ubyte.gz http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
 curl --create-dirs -o data/t10k-labels-idx1-ubyte.gz http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
-gzip data/*
+gzip -d data/*
 
